6b28d33b9e3a41f63ab2a5608b6a691090f83bad
Taylor Britton <tbrit10@gmail.com>
2021-10-14 22:52:51 -0700

    Catchup commit
    Catchup a lot of work like adding buttons and making the code cleaner
    and adding comments and stuff

--- timer-7seg-led.ino
+++ timer-7seg-led.ino
@@ -1,86 +1,185 @@
 #include <arduino-timer.h>
-#include <Wire.h> // Enable this line if using Arduino Uno, Mega, etc.
 #include <Adafruit_GFX.h>
-#include "Adafruit_LEDBackpack.h"
+#include <AceButton.h>
 
+#include <LowPower.h>
+#include <Adafruit_LEDBackpack.h>
+#include <pitches.h>
+
+using namespace ace_button;
+
+void handleStopButton(AceButton* /*button*/, uint8_t eventType, uint8_t /*buttonState*/);
+void handleAddTimeButton(AceButton* /*button*/, uint8_t eventType, uint8_t /*buttonState*/);
+void handleTimer1Button(AceButton* /*button*/, uint8_t eventType, uint8_t /*buttonState*/);
+void handleTimer2Button(AceButton* /*button*/, uint8_t eventType, uint8_t /*buttonState*/);
+void handleTimer3Button(AceButton* /*button*/, uint8_t eventType, uint8_t /*buttonState*/);
+
+// Timer values
+const int ADD_TIME_SECONDS = 30;
+const int TIMER1_SECONDS = 60;
+const int TIMER2_SECONDS = 120;
+const int TIMER3_SECONDS = 3600;
+
+// Pin values
+const int ALARM_PIN = 9;
+const int STOP_BUTTON_PIN = 7;
+const int ADD_TIME_BUTTON_PIN = 8; 
+const int START_TIMER1_PIN = 6;
+const int START_TIMER2_PIN = 5;
+const int START_TIMER3_PIN = 4;
+const int WAKEUP_PIN = 10;
+
+// Other settings
+const int BRIGHTNESS = 3;   // Valid values: 1-10
+const int BLINK_RATE = 1;   // Valid values: 0-3
+const int BLINK_TIME = 30;  // The amount of seconds left in the timer when the display should begin blinking
 const int ALARM_LENGTH = 5; // 5 second alarm at the end of the timer
+const int ALARM_TONE = NOTE_C5; // TODO: Make a tone tester and find a nice tone for the alarm
+const int ALARM_TONE_LENGTH = 300; // 300 ms chirp 
 
-int timer = 60;
+// Members
+int timer = 0; // The all-mighty timer (value in seconds)
 
-int buttonPin = 16; 
+// Tracks the amount of alarm chirps that have been made. 
+// Initialized to ALARM_LENGTH so our make_tone function doesn't have to initialize it the first time the alarm is called
+int alarmLength = ALARM_LENGTH; 
 
-bool shouldBlink = false; // Flag determining if we should blink the display when the timer reaches blinkTime seconds left
-int blinkTime = 30; // The amount of seconds left in the timer when the display should begin blinking
-int blinkRate = 1; // Speed of display blinkage
+bool shouldBlink = false;   // Flag determining if we should blink the display when the timer reaches blinkTime seconds left
+bool isBlinking = false;    // Flag tracking if we are already blinking
+bool makeAlarm = false;     // Flag determining if we should make a tone to indicate the timer is done
+bool timerRunning = false;  // Flag determining if there is currently a timer running
 
-int alarmPin = 8;
-int alarmTone = 523; // TODO: Make a tone tester and find a nice tone for the alarm
-int alarmToneLength = 300; // 300 ms chirp 
-bool makeAlarm = false;
+auto actionTimer = timer_create_default();  // Main "timer" timer
+auto alarmTimer = timer_create_default();   // Timer for the alarm. Shorter period = faster alarm
 
-auto actionTimer = timer_create_default(); // create a timer with default settings
-auto alarmTimer = timer_create_default(); // create a timer with default settings
+Adafruit_7segment matrix = Adafruit_7segment(); // 7-seg LED driver
 
-Adafruit_7segment matrix = Adafruit_7segment();
+// Button setup
+AceButton stopButton(STOP_BUTTON_PIN);
+AceButton addTimeButton(ADD_TIME_BUTTON_PIN);
+AceButton timer1Button(START_TIMER1_PIN);
+AceButton timer2Button(START_TIMER2_PIN);
+AceButton timer3Button(START_TIMER3_PIN);
 
 void setup() {
-  pinMode(buttonPin, INPUT);
+  // Pin setup
+  pinMode(STOP_BUTTON_PIN, INPUT_PULLUP);
+  pinMode(ADD_TIME_BUTTON_PIN, INPUT_PULLUP);
+  pinMode(START_TIMER1_PIN, INPUT_PULLUP);  
+  pinMode(START_TIMER2_PIN, INPUT_PULLUP);
+  pinMode(START_TIMER3_PIN, INPUT_PULLUP);
+
+  // Button event handler binding
+  ButtonConfig* config = ButtonConfig::getSystemButtonConfig();
+  config->setEventHandler(buttonHandler);
+
+  // LED setup
   matrix.begin(0x70);
-  matrix.setBrightness(5);
+  matrix.setBrightness(BRIGHTNESS);
   matrix.drawColon(true);
+
+  // Timer setup
   actionTimer.every(1000, dec_timer);
   alarmTimer.every(1000, make_tone);
-
 }
 
-int lastButtonState = 0;     // previous state of the button
 void loop() {
-  
-  int buttonState = digitalRead(buttonPin);
-  if (buttonState != lastButtonState) {
-    // Check for button press
-    if (buttonState == LOW)
-    {
-      // Add 30 seconds to timer
-      timer += 30;
-      write_timer();
-    }
-    lastButtonState = buttonState;
-  }
-  
+  // Check if any buttons were pressed
+  stopButton.check();
+  addTimeButton.check();
+  timer1Button.check();
+  timer2Button.check();
+  timer3Button.check();
+
+  // Tick timers
   actionTimer.tick();
   alarmTimer.tick();
 }
 
+void buttonHandler(AceButton* button, uint8_t eventType, uint8_t buttonState) {
+  // Exit early if the event isn't a keypress
+  if(eventType != AceButton::kEventPressed) { return; }
+  switch (button->getPin()) {
+    case STOP_BUTTON_PIN:
+      Serial.println("Stop button pressed");
+      timer = 0;
+      timerRunning = false;
+      stop_blink();
+      write_blank();
+      break;
+     case ADD_TIME_BUTTON_PIN:
+      Serial.println("Add Time button pressed");
+      timer += ADD_TIME_SECONDS;
+      if(timerRunning == false) {
+        timerRunning = true;
+      }
+      write_timer();
+      break;
+     case START_TIMER1_PIN:
+      Serial.println("Timer 1 button pressed");
+      // Add time to to timer
+      timer = TIMER1_SECONDS;
+      // If the timer was stopped, start it again
+      if(timerRunning == false) {
+        timerRunning = true;
+      }
+      write_timer();
+      break;
+     case START_TIMER2_PIN:
+      Serial.println("Timer 2 button pressed");
+      // Add time to to timer
+      timer = TIMER2_SECONDS;
+      // If the timer was stopped, start it again
+      if(timerRunning == false) {
+        timerRunning = true;
+      }
+      write_timer();
+      break;
+     case START_TIMER3_PIN:
+      Serial.println("Timer 3 button pressed");
+      // Add time to to timer
+      timer = TIMER3_SECONDS;
+      // If the timer was stopped, start it again
+      if(timerRunning == false) {
+        timerRunning = true;
+      }
+      write_timer();
+      break;
+  }
+}
+
 // Do some checks, subtract 1 second from our timer, and then display it
 bool dec_timer(void *) {
+  // Exit early if the alarm is not running (paused or done)
+  if(timerRunning == false) return true;
   if(timer == 0) {
     // Exit early if timer has finished
     // and stop blinking
-    matrix.blinkRate(0);
-    make_alarm(ALARM_LENGTH);
-
-    return false; // do not repeat once timer hits zero
+    make_alarm();
+    stop_blink();
+    write_blank();
+    timerRunning = false;
+    return true;
+  }
+  timer -= 1; // dec timer
+      
+  // Start blinking if only BLINK_TIME seconds are left
+  if(shouldBlink && timer < BLINK_TIME) {
+      start_blink();
   }
   
-  //Serial.print("Timer value: ");
-  //Serial.println(timer);
-  timer -= 1; // dec timer
   write_timer();
-
-  return true; // timer isn't done, repeat
+  return true;
 }
 
 
-// Make an alarm noise "length" amount of times
-int alarmLength = ALARM_LENGTH;
-void make_alarm(int length) {
-  alarmLength = length;
-  makeAlarm = true;
-}
+/////
+// ALARM HELPERS
+////
 
 // Make an alarm tone
 // Tone will only play if makeAlarm is true
+// Called every second by alarmTimer in loop()
 bool make_tone(void *) {
   // Exit early if we arn't meant to make an alarm
   if(makeAlarm == false) return true;
@@ -91,29 +190,52 @@ bool make_tone(void *) {
     makeAlarm = false;
   }
   // If we get here, make a noise
-  tone(alarmPin, alarmTone, alarmToneLength);
+  tone(ALARM_PIN, ALARM_TONE, ALARM_TONE_LENGTH);
   // Repeat forvever
   return true;
 }
 
+// Tell make_tone to start making an alarm
+// The alarm will sound ALARM_LENGTH amount of times
+void make_alarm() {
+  alarmLength = ALARM_LENGTH;
+  makeAlarm = true;
+}
+
+////
+// BLINK HELPERS
+////
+
+void start_blink() {
+  if(isBlinking == false) {
+    matrix.blinkRate(BLINK_RATE);
+    isBlinking = true;
+  }
+}
+
+void stop_blink() {
+  if(isBlinking) {
+    matrix.blinkRate(0);
+    isBlinking = false;
+  }
+}
+
+////
+// 7-SEG LED SCREEN HELPERS
+////
 
 // Convert the current timer value (in seconds) into a clock display "MM:SS" and writes it to the display
 void write_timer() {
-  // If the timer is too high to draw, just write something silly
-  if(timer > 3600) {
-     write_error();
-     return;
+  // If the timer is too high to draw as MM:SS, draw as HH:MM instead
+  if(timer > 3600) {    
+     write_hour_minutes(timer);
   } else {
-    // make sure we draw the colon (this is sometimes turned off when displaying "Err" message)
-      matrix.drawColon(true);
+    write_minutes_seconds(timer);
   }
-  
-  // Start blinking if only 30 seconds are left
-  if(timer < blinkTime && shouldBlink) {
-      matrix.blinkRate(blinkRate);
-  }
+}
+
+void write_minutes_seconds(int seconds) {
   int minutes = 0;
-  int seconds = timer;
   
   if(timer > 60) {
     minutes = timer / 60;
@@ -128,6 +250,36 @@ void write_timer() {
   matrix.writeDigitNum(1, minutes % 10);
   matrix.writeDigitNum(3, seconds / 10);
   matrix.writeDigitNum(4, seconds % 10);
+  matrix.writeDisplay();
+}
+
+// Writes hours/minutes as HH:MM
+void write_hour_minutes(int seconds) {
+  int hours = seconds / (60 * 60);
+  int minutes = seconds / (60) % 60;
+  matrix.writeDigitNum(0, hours / 10);
+  matrix.writeDigitNum(1, hours % 10);
+  matrix.writeDigitNum(3, minutes / 10);
+  matrix.writeDigitNum(4, minutes % 10);
+  matrix.writeDisplay();
+}
+
+// Writes hours/minutes as h.m'hr' where h is the number of hours and m is the single-decimal percision percent of minutes in an hour
+void write_hour_minutes2(int seconds) {
+  // Show error if hours > 10 (we cannot display this in a 4 character array if we want to include Hr
+  if(seconds > 36000) {
+    write_error();
+    return;
+  }
+  
+  matrix.drawColon(false);
+  // Should read "1.5hr"
+  int hours = seconds / (60 * 60);
+  int minutes = seconds / (60) % 60;
+  matrix.writeDigitNum(0, hours % 10, true); // H.
+  matrix.writeDigitNum(1, 60 / minutes % 10); // M
+  matrix.writeDigitRaw(3, B01110100); //h
+  matrix.writeDigitRaw(4, B01010000); //r
   matrix.writeDisplay();
 }
 
@@ -147,4 +299,6 @@ void write_blank() {
   matrix.writeDigitRaw(1, B00000000); // Nothing
   matrix.writeDigitRaw(3, B00000000); // Nothing
   matrix.writeDigitRaw(4, B00000000); // Nothing
+  matrix.drawColon(false);
+  matrix.writeDisplay();   
 }
