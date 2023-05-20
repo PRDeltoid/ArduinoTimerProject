dfd5f51c23d23c6bea63a0131222a33b871656dd
Taylor Britton <tbrit10@gmail.com>
2023-05-20 14:09:01 -0700

    Add tone playing on timer completion

--- timer-7seg-led.ino
+++ timer-7seg-led.ino
@@ -6,55 +6,100 @@
 
 using namespace ace_button;
 
+struct Note {
+  int Note;
+  int Duration;
+  int PauseAfter;
+};
+
+class TonePlayer {
+  private:
+    int currNote = 0;
+    int currPause = 0;
+    int numNotes = 0;
+    int alarmPin;
+    bool isRunning = false;
+    const Note* notes;
+
+  public:
+    TonePlayer(const Note notes[], int alarmPin) {
+      this->notes = notes;
+      this->numNotes = sizeof(notes)/sizeof(Note);
+      this->alarmPin = alarmPin;
+    }
+  
+    void tick(int ticks) {
+      // Exit early if the tone player is not running
+      if(isRunning == false or ticks < 1) { return; }
+      
+      currPause -= ticks;
+      
+      if(currPause < 0) {
+        tone(alarmPin, notes[currNote].Note, notes[currNote].Duration);
+
+        currPause = notes[currNote].PauseAfter;
+        currNote++;
+
+        if(currNote >= numNotes) {
+          stop();
+        }
+      }    
+    }
+  
+    void stop() {
+      isRunning = false;
+    }
+    
+    void activate() {
+      isRunning = true;
+      currNote = 0;
+    }
+};
+
 ////////////
 // SETTINGS
 ////////////
 
+// Alarm notes
+const Note notes[] = {
+  { NOTE_C7, 200, 220 },
+  { NOTE_C7, 200, 220 },
+  { NOTE_C7, 200, 300 },
+  { NOTE_C7, 200, 220 },
+  { NOTE_C7, 200, 220 },
+  { NOTE_C7, 200, 0 },
+};
+ 
 // Timer values
-const int ADD_TIME_SECONDS = 5 * 60; // 5 minutes
-const int TIMER1_SECONDS = 10 * 60; // 10 minutes
-const int TIMER2_SECONDS = 45 * 60; // 45 minutes
+const int ADD_TIME_SECONDS = 5 * 60;  // 5 minutes
+const int TIMER1_SECONDS = 10 * 60;   // 10 minutes
+const int TIMER2_SECONDS = 45 * 60;   // 45 minutes
 
 // Pin values
 const int ALARM_PIN = 6;
 const int STOP_BUTTON_PIN = 1;
 const int PAUSE_BUTTON_PIN = 0;
-const int ADD_TIME_BUTTON_PIN = 8; 
+const int ADD_TIME_BUTTON_PIN = 7; 
 const int START_TIMER1_PIN = 3;
 const int START_TIMER2_PIN = 2;
 
 // Other settings
-const int BRIGHTNESS = 3;   // Valid values: 1-10
-const int BLINK_RATE = 1;   // Valid values: 0-3
-const int BLINK_TIME = 30;  // The amount of seconds left in the timer when the display should begin blinking
-const int ALARM_LENGTH = 5; // 5 second alarm at the end of the timer
-const int ALARM_TONE = NOTE_C5; // TODO: Make a tone tester and find a nice tone for the alarm
-const int ALARM_TONE_LENGTH = 300; // 300 ms chirp 
+const int BRIGHTNESS = 3;       // Valid values: 1-10
+const int BLINK_RATE = 1;       // Valid values: 0-3
+const int BLINK_TIME = 30;      // The amount of seconds left in the timer when the display should begin blinking
+const bool SHOULD_BLINK = false;// Flag determining if we should blink the display when the timer reaches blinkTime seconds left
 
 // MEMBERS
-int timer = 0; // The all-mighty timer (value in seconds)
+int timer = 0;                // The all-mighty timer (value in seconds) 
+bool isBlinking = false;      // Flag tracking if we are already blinking
+bool timerRunning = false;    // Flag determining if there is currently a timer running
+bool stopwatchRunning = false;// Flag determining if there is a stopwatch running
+bool paused = false;          // Flag determining if the timer is set but paused
+int prevTicks = 1000;         // Tick tracker for the tone player to piggy back on actionTimer's ticks
 
-// Tracks the amount of alarm chirps that have been made. 
-// Initialized to ALARM_LENGTH so our make_tone function doesn't have to initialize it the first time the alarm is called
-int alarmLength = ALARM_LENGTH; 
-
-bool shouldBlink = false;   // Flag determining if we should blink the display when the timer reaches blinkTime seconds left
-bool isBlinking = false;    // Flag tracking if we are already blinking
-bool makeAlarm = false;     // Flag determining if we should make a tone to indicate the timer is done
-bool timerRunning = false;  // Flag determining if there is currently a timer running
-bool paused = false;        // Flag determining if the timer is set but paused
-
-auto actionTimer = timer_create_default();  // Main "timer" timer
-auto alarmTimer = timer_create_default();   // Timer for the alarm. Shorter period = faster alarm
-
+auto actionTimer = timer_create_default();      // Main "timer" timer
 Adafruit_7segment matrix = Adafruit_7segment(); // 7-seg LED driver
-
-// Button setup
-void handleStopButton(AceButton* /*button*/, uint8_t eventType, uint8_t /*buttonState*/);
-void handlePauseButton(AceButton* /*button*/, uint8_t eventType, uint8_t /*buttonState*/);
-void handleAddTimeButton(AceButton* /*button*/, uint8_t eventType, uint8_t /*buttonState*/);
-void handleTimer1Button(AceButton* /*button*/, uint8_t eventType, uint8_t /*buttonState*/);
-void handleTimer2Button(AceButton* /*button*/, uint8_t eventType, uint8_t /*buttonState*/);
+TonePlayer player(notes, ALARM_PIN);            // Passive buzzer tone player
 
 AceButton stopButton(STOP_BUTTON_PIN);
 AceButton pauseButton(PAUSE_BUTTON_PIN);
@@ -81,7 +126,6 @@ void setup() {
 
   // Timer setup
   actionTimer.every(1000, dec_timer);
-  alarmTimer.every(1000, make_tone);
 }
 
 void loop() {
@@ -94,7 +138,9 @@ void loop() {
   
   // Tick timers
   actionTimer.tick();
-  alarmTimer.tick();
+  int ticks = actionTimer.tick();
+  player.tick(prevTicks - ticks);
+  prevTicks = ticks;
 }
 
 void buttonHandler(AceButton* button, uint8_t eventType, uint8_t buttonState) {
@@ -107,44 +153,55 @@ void buttonHandler(AceButton* button, uint8_t eventType, uint8_t buttonState) {
       break;
      case PAUSE_BUTTON_PIN:
       Serial.println("Pause button pressed");
-      pause_or_play();
+      if(stopwatchRunning || timerRunning) {
+        pause_or_play();
+      } else {
+        start_stopwatch();
+      }
       break;
      case ADD_TIME_BUTTON_PIN:
       Serial.println("Add Time button pressed");
-      add_time(ADD_TIME_SECONDS);
+      if(stopwatchRunning) return;
+      start_timer(timer + ADD_TIME_SECONDS);
       break;
      case START_TIMER1_PIN:
-      Serial.println("Timer 1 button pressed");
+      Serial.println("Timer 1 button pressed");      
+      if(stopwatchRunning) return;
       start_timer(TIMER1_SECONDS);
       break;
      case START_TIMER2_PIN:
       Serial.println("Timer 2 button pressed");
+      if(stopwatchRunning) return;
       start_timer(TIMER2_SECONDS);
       break;
   }
 }
 
-void add_time(int timeSeconds) {
-  timer += timeSeconds;
-  if(timerRunning == false) {
-    timerRunning = true;
-    matrix.drawColon(true);
-  }
-}
-
 void start_timer(int timerSeconds) {
-  // Add time to the timer
   timer = timerSeconds;
+
   // If the timer was stopped, start it again
   if(timerRunning == false) {
     timerRunning = true;
     matrix.drawColon(true);
   }
+  write_timer();
+}
+
+void start_stopwatch() {
+  if(stopwatchRunning == false) {
+    timer = 0;
+    stopwatchRunning = true;
+    matrix.drawColon(true);
+  }
+  write_timer();
 }
 
 void stop_timer() {
   timer = 0;
   timerRunning = false;
+  stopwatchRunning = false;
+  paused = false;
   stop_blink();
   write_blank();
 }
@@ -161,55 +218,30 @@ void pause_or_play() {
 // Do some checks, subtract 1 second from our timer, and then display it
 bool dec_timer(void *) {
   // Exit early if the alarm is not running (paused or done)
-  if(timerRunning == false || paused) return true;
-  if(timer == 0) {
-    // Exit early if timer has finished
-    // and stop blinking
-    make_alarm();
-    stop_blink();
-    write_blank();
-    timerRunning = false;
-    return true;
-  }
-  timer -= 1; // dec timer
-      
-  // Start blinking if only BLINK_TIME seconds are left
-  if(shouldBlink && timer < BLINK_TIME) {
-      start_blink();
+  if(timerRunning == false && stopwatchRunning == false || paused) return true;
+
+  if(timerRunning) {
+    if(timer == 0) {
+      // Exit early if timer has finished
+      // and stop blinking
+      player.activate();
+      stop_blink();
+      write_blank();
+      timerRunning = false;
+      return true;
+    }
+    timer -= 1; // dec timer
+        
+    // Start blinking if only BLINK_TIME seconds are left
+    if(SHOULD_BLINK && timer < BLINK_TIME) {
+        start_blink();
+    }
+  } else if(stopwatchRunning) {
+    timer += 1;
   }
   
   write_timer();
   return true;
-}
-
-/////
-// ALARM HELPERS
-////
-
-// Make an alarm tone
-// Tone will only play if makeAlarm is true
-// Called every second by alarmTimer in loop()
-bool make_tone(void *) {
-  // Exit early if we arn't meant to make an alarm
-  if(makeAlarm == false) return true;
-  // We made a chirp. Minus one from the chirp counter
-  alarmLength--;
-  // We made all of our chirps. Turn off the tone maker
-  if(alarmLength == 0) {
-    makeAlarm = false;
-  }
-  
-  // If we get here, make a noise
-  tone(ALARM_PIN, ALARM_TONE, ALARM_TONE_LENGTH);
-  // Repeat forvever
-  return true;
-}
-
-// Tell make_tone to start making an alarm
-// The alarm will sound ALARM_LENGTH amount of times
-void make_alarm() {
-  alarmLength = ALARM_LENGTH;
-  makeAlarm = true;
 }
 
 ////
@@ -244,7 +276,7 @@ void write_timer() {
   }
 }
 
-// Writes minutes/seconds as MM:SS
+// Writes minutes/seconds as mm:SS
 void write_minutes_seconds(int seconds) {
   int minutes = 0;
   
@@ -257,18 +289,32 @@ void write_minutes_seconds(int seconds) {
   // loc 2 is the colon dots
   // loc 4 is further right (lower value)
   // Extract the first and second digits from both values 
-  matrix.writeDigitNum(0, minutes / 10);
+
+  // Only draw the leading digit if it is non-zero
+  int leadingDigit = minutes / 10;
+  if(leadingDigit > 0) {
+      matrix.writeDigitNum(0, minutes / 10);
+  } else {
+      matrix.writeDigitRaw(0, B00000000); // Nothing
+  }
   matrix.writeDigitNum(1, minutes % 10);
   matrix.writeDigitNum(3, seconds / 10);
   matrix.writeDigitNum(4, seconds % 10);
   matrix.writeDisplay();
 }
 
-// Writes hours/minutes as HH:MM
+// Writes hours/minutes as hh:MM
 void write_hour_minutes(int seconds) {
   int hours = seconds / (60 * 60);
   int minutes = seconds / (60) % 60;
-  matrix.writeDigitNum(0, hours / 10);
+
+  // Only draw the leading digit if it is non-zero
+  int leadingDigit = hours / 10;
+  if(leadingDigit > 0) {
+      matrix.writeDigitNum(0, hours / 10);
+  } else {
+      matrix.writeDigitRaw(0, B00000000); // Nothing
+  }
   matrix.writeDigitNum(1, hours % 10);
   matrix.writeDigitNum(3, minutes / 10);
   matrix.writeDigitNum(4, minutes % 10);
